51add4df214bde78f6cf267010a88a5e82554018
Ivan Konnikov <ivankonnikov@MacBook-Pro-2.local>
2023-06-08 21:47:57 +0200

    feature/add-expenses: Fixed DB connection, created expenses list component.

---
+++ src/components/HomeTabPanel/ListedExpenseHistoryTab.tsx
@@ -0,0 +1,23 @@
+import { Box, Center, CircularProgress, Text } from "@chakra-ui/react";
+import useGetExpenses from "../../hooks/useGetExpenses";
+import { Expense } from "../../types";
+import { ExpenseCard } from "./ExpenseCard";
+
+export const ListedExpenseHistoryTab: React.FC = () => {
+  const { data: expenses, isLoading } = useGetExpenses();
+
+  const getSortedExpenses = (expenses: Expense[]) =>
+    [...expenses].sort((a, b) => b.date.seconds - a.date.seconds);
+
+  return (
+    <Center>
+      <Box maxHeight="30em" overflow="auto" overflowY="auto">
+        {expenses &&
+          getSortedExpenses(expenses).map((expense: Expense) => {
+            return <ExpenseCard expense={expense} />;
+          })}
+        {isLoading && <CircularProgress isIndeterminate color="blue.400" />}
+      </Box>
+    </Center>
+  );
+};
